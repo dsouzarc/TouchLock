fa1f2bb1639aed46fa25b6a6e4a9cc39157676fe
Ryan D'souza <dsouzarc@gmail.com>
2017-04-05 01:28:39 -0400

    Removed YAP database - began work on Constants

---
+++ MessagesExtension/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  TouchLock
+//
+//  Created by Ryan D'souza on 4/5/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+@end
---
+++ MessagesExtension/Constants.m
@@ -0,0 +1,13 @@
+//
+//  Constants.m
+//  TouchLock
+//
+//  Created by Ryan D'souza on 4/5/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+@end
