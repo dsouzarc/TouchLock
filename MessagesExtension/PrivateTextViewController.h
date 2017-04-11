bfdcab1d436cc8213df577eb79fb40753ccacd32
Ryan D'souza <dsouzarc@gmail.com>
2017-04-11 17:52:05 -0400

    UIFloatLabelTextView library

---
+++ MessagesExtension/PrivateTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrivateTextViewController.h
+//  TouchLock
+//
+//  Created by Ryan D'souza on 4/11/17.
+//  Copyright © 2017 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PrivateTextViewController : UIViewController
+
+@end
